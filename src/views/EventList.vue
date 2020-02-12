177b3a4baca996a3f8b00921a13eb80fdccc9108
brn0 <brunomengaldo@gmail.com>
2020-02-12 09:33:48 -0300

    começar a ver vuex

---
+++ src/views/EventList.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>Lista de Eventos :]</h1>
+        <EventCard v-for="event in events" :key="event.id" :event="event"/>
+    </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
+
+export default {
+    components: {
+        EventCard
+    },
+    data() {
+        return {
+            events: []
+        }
+    },
+    created() {
+        EventService.getEvents()
+            .then(response => {
+                this.events = response.data // <--- set the events data
+            })
+            .catch(error => {
+                console.log('Deu ruim:', error.response)
+            })
+    }
+}
+</script>
+
+<style scoped></style>
